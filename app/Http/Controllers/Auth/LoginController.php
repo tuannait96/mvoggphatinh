540fc57755f16ef64b812672c6f1314d56fb8365
NGUYEN THACH <peternguyen0293@gmail.com>
2020-09-28 16:49:35 +0700

    update ridirect affter login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,19 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+
+    //function direct affter login
+    protected function redirectTo()
+    {
+        if (Auth::user()->roleid==1) {
+            //dd();
+            return '/admin';
+        }
+        return '/home';
+    }
+
+
+    //protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
